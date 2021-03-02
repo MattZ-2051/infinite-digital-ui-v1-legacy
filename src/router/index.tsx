cccaa6ef73ca394ec19bdf6a12e462cc9ed1015d
Pablo Noguera <pnoguera.dev@gmail.com>
2021-03-02 20:11:07 -0300

    Bug fix - remove duplicated router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,8 @@
 import {
-  BrowserRouter as Router,
   Switch,
   Route,
-  Redirect,
 } from "react-router-dom";
+
 import Landing from "../views/Landing";
 import UserAccount from "../views/UserAccount";
 
@@ -11,14 +10,14 @@ export interface IProps {
   children?: any;
 }
 
-const RouterComponent: React.FC<IProps> = () => {
+const RouterComponent = () => {
   return (
-    <Router>
+    <>
       <Switch>
         <Route exact path="/" component={Landing} />
-        <Route exact path="/user-account" component={UserAccount} />
+        <Route path="/user-account" component={UserAccount} />
       </Switch>
-    </Router>
+    </>
   );
 };
 
