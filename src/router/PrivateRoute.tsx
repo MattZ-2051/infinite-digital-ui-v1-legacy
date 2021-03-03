a082ee1ca18c50059ad1289d063ccfae69d2e058
Pablo Noguera <pnoguera.dev@gmail.com>
2021-03-02 21:23:01 -0300

    Add authorization components

--- src/router/PrivateRoute.tsx
+++ src/router/PrivateRoute.tsx
@@ -1,7 +1,28 @@
-export interface IProps {}
+import { Route, Redirect } from "react-router-dom";
+import { getToken } from "lib/utils/auth";
 
-const PrivateRoute: React.FC<IProps> = () => {
-  return <div>PrivateRoute</div>;
+const PrivateRoute = ({ component: Component, ...rest }): any => {
+  const token = getToken();
+
+  return (
+    <Route
+      {...rest}
+      render={(routeProps) => {
+        if (token) {
+          return <Component {...routeProps} />;
+        }
+
+        return ( //TODO: revisar porque uso un modal
+          <Redirect
+            to={{
+              pathname: "/sign-in",
+              state: { from: routeProps.location },
+            }}
+          />
+        );
+      }}
+    />
+  );
 };
 
 export default PrivateRoute;
