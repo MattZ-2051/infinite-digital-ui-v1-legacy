eec1094674802e8bc48323d800a37273ec894d97
Pablo Noguera <pnoguera.dev@gmail.com>
2021-03-03 09:42:17 -0300

    Add footer base styles

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,25 @@
+import styled from "styled-components/macro";
 export interface IProps {}
 
 const Footer: React.FC<IProps> = () => {
-  return <footer>Footer</footer>;
+  return (
+    <StyledFooter>
+      Copyright © 2020 Citizens Reserve, Inc. All Rights Reserved.
+    </StyledFooter>
+  );
 };
+
+const StyledFooter = styled.footer`
+  height: 55px;
+  background-color: black;
+  color: white;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  font-size: 12px;
+  display: flex;
+  align-items: center;
+  padding: 0 50px 0 50px;
+`;
 
 export default Footer;
