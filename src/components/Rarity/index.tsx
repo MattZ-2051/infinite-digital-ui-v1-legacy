5d7be871ec3708cde358f471eb563748e06a4191
Matt Zamora <mzamora2899@gmail.com>
2021-03-21 14:47:09 -0600

    SukuLab/infinite-digital-ui#8 add types for rarity

--- src/components/Rarity/index.tsx
+++ src/components/Rarity/index.tsx
@@ -1,15 +1,35 @@
 import styled from 'styled-components';
 
 export interface IProps {
-  type: string;
+  rarityType: string;
 }
 
-const Rarity = ({ type }: IProps) => {
+const Rarity = ({ rarityType }: IProps) => {
   return (
     <>
-      {type === 'common' && (
+      {rarityType === 'common' && (
         <StyledDiv>
-          <p>Common</p>
+          <p style={{ fontSize: '12px' }}>COMMON</p>
+        </StyledDiv>
+      )}
+      {rarityType === 'rare' && (
+        <StyledDiv color='#25E29E'>
+          <p style={{ fontSize: '12px', color: 'black' }}>RARE</p>
+        </StyledDiv>
+      )}
+      {rarityType === 'uncommon' && (
+        <StyledDiv color='#AAAAAA'>
+          <p style={{ fontSize: '0.75rem', color: 'black' }}>UNCOMMON</p>
+        </StyledDiv>
+      )}
+      {rarityType === 'legendary' && (
+        <StyledDiv color='#FCB955'>
+          <p style={{ fontSize: '0.75rem', color: 'black' }}>LEGENDARY</p>
+        </StyledDiv>
+      )}
+      {rarityType === 'epic' && (
+        <StyledDiv color='#D57BFF'>
+          <p style={{ fontSize: '1rem', color: 'black' }}>EPIC</p>
         </StyledDiv>
       )}
     </>
@@ -17,7 +37,7 @@ const Rarity = ({ type }: IProps) => {
 }
 
 const StyledDiv = styled.div`
-  background-color: white;
+  background-color: ${props => props.color || 'white'};
   color: var(--grey-40);
   height: 50%;
   height: 23px;
