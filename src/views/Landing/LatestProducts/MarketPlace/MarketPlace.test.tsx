dc89340303f5c5568301cb9da33c0c8e992af383
Matt Zamora <mzamora2899@gmail.com>
2021-03-22 21:48:36 -0600

    test

--- src/views/Landing/LatestProducts/MarketPlace/MarketPlace.test.tsx
+++ src/views/Landing/LatestProducts/MarketPlace/MarketPlace.test.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { render, screen, cleanup, fireEvent } from "@testing-library/react";
+import MarketPlace from './index';
+import { Provider } from "react-redux";
 
 // cleanup after each test is run
 // cleanup unmounts everything from the DOM after tests are run
 afterEach(cleanup)
 
-test("", () => {
+test("<MarketPlace />", () => {
 
 
+  // const { debug, container } = render(<Provider><MarketPlace /></Provider>);
+  // console.log(container)
   // debug();
 
   expect(true).toBeTruthy();
