3fed82123a2d77a3adc73d7526908dd28032637d
Matt Zamora <mzamora2899@gmail.com>
2021-03-22 18:50:36 -0600

    SukuLab/infinite-digital-ui#8 add more tests

--- src/components/ProductPanel/DropBoxPanel/DropBoxPanel.test.tsx
+++ src/components/ProductPanel/DropBoxPanel/DropBoxPanel.test.tsx
@@ -1,11 +1,21 @@
 import React from "react";
-import { render, screen, cleanup } from "@testing-library/react";
+import { render, screen, cleanup, fireEvent } from "@testing-library/react";
 import DropBoxPanel from './index';
 
-test("<DropBoxPanel />", () => {
-  const { debug } = render(<DropBoxPanel />);
+// cleanup after each test is run
+// cleanup unmounts everything from the DOM after tests are run
+afterEach(cleanup)
 
-  debug();
+test("<DropBoxPanel />", () => {
+
+  // Renders Component
+  const { debug, getByTestId } = render(<DropBoxPanel />);
+
+  // Expect Content Container of Panel to be a div
+  expect(getByTestId('panel-content').tagName).toBe('DIV')
+
+
+  // debug();
 
   expect(true).toBeTruthy();
 })
