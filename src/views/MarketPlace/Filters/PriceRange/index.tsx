424ed7763712ed1aa918a912824d18ba14a41d38
Matt Zamora <mzamora2899@gmail.com>
2021-03-31 12:39:24 -0600

    rename files

---
+++ src/views/MarketPlace/Filters/PriceRange/index.tsx
@@ -0,0 +1,88 @@
+import React, { useEffect } from 'react';
+import Slider from '@material-ui/core/Slider';
+import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
+
+// function valuetext(value) {
+//   return `${value}°C`;
+// }
+
+export interface IProps {
+  handleFilter: (name: string, data: any) => void;
+  defaultFilter: number[];
+}
+
+const RangeFilter: React.FC<IProps> = ({ handleFilter, defaultFilter }) => {
+  const [value, setValue] = React.useState<number[]>([10, 50]);
+
+  useEffect(() => {
+    if (defaultFilter.length) {
+      return setValue([...defaultFilter]);
+    } else {
+      return setValue([10, 50]);
+    }
+  }, [defaultFilter]);
+
+  const handleChange = (event: any, newValue: number | number[]) => {
+    setValue(newValue as number[]);
+  };
+
+  const handleCommit = () => {
+    handleFilter('price', [...value]);
+  };
+
+  return (
+    <div style={{ width: '278px' }}>
+      <Typography id="range-slider" gutterBottom >
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          <span style={{ fontSize: '18px', fontWeight: 400, color: '#9E9E9E' }}>Price Range</span>
+          <span style={{ color: 'black', fontSize: '14px' }}>From ${value[0]} to ${value[1]}</span>
+        </div>
+      </Typography>
+      <StyledSlider
+        value={value}
+        onChange={handleChange}
+        valueLabelDisplay="auto"
+        aria-labelledby="range-slider"
+        max={1000}
+        min={0}
+      />
+    </div>
+  );
+};
+
+const StyledSlider = styled(Slider)`
+  .MuiSlider-root {
+    color: black;
+  }
+  .MuiSlider-thumb {
+    border: 2px solid black;
+    color: white;
+    width: 15px;
+    height: 15px;
+    box-shadow: none;
+  }
+
+  .MuiSlider-thumb:hover {
+    color: black;
+    box-shadow: 0px 0px 0px 8px rgb(0 0 0 / 16%);
+  }
+
+  .MuiSlider-track {
+    color: black;
+  }
+
+  .MuiSlider-rail {
+    background-color: black;
+  }
+
+  .MuiSlider-valueLabel	{
+    color: black;
+  }
+
+  .Mui-focusVisible	{
+    color: black;
+  }
+`;
+
+export default RangeFilter;
