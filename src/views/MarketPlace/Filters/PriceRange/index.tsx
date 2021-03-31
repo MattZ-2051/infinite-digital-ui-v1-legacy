fc714c7d0fc41f37a13796478fed906263c03bd3
Matt Zamora <mzamora2899@gmail.com>
2021-03-31 13:08:13 -0600

    rename open variable

--- src/views/MarketPlace/Filters/PriceRange/index.tsx
+++ src/views/MarketPlace/Filters/PriceRange/index.tsx
@@ -16,7 +16,7 @@ export interface IProps {
 
 const RangeFilter: React.FC<IProps> = ({ handleFilter, defaultFilter }) => {
   const [value, setValue] = React.useState<number[]>([0, 1000]);
-  const [open, setOpen] = React.useState<boolean | undefined>(true);
+  const [isHidden, setIsHidden] = React.useState<boolean | undefined>(true);
 
   useEffect(() => {
     if (defaultFilter.length) {
@@ -31,7 +31,7 @@ const RangeFilter: React.FC<IProps> = ({ handleFilter, defaultFilter }) => {
   };
 
   const handleOpen = () => {
-    setOpen(!open)
+    setIsHidden(!isHidden)
   }
   const handleCommit = () => {
     handleFilter('price', [...value]);
@@ -43,10 +43,10 @@ const RangeFilter: React.FC<IProps> = ({ handleFilter, defaultFilter }) => {
         <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }} onClick={handleOpen}>
           <span style={{ fontSize: '18px', fontWeight: 500, color: '#9E9E9E' }}>Price Range</span>
           <span style={{ color: 'black', fontSize: '14px' }}>From ${value[0]} to ${value[1]}</span>
-          {open ? <KeyboardArrowDownIcon /> : <KeyboardArrowUpIcon />}
+          {isHidden ? <KeyboardArrowDownIcon /> : <KeyboardArrowUpIcon />}
         </div>
       </Typography>
-      <div hidden={open}>
+      <div hidden={isHidden}>
         <StyledSlider
           value={value}
           onChange={handleChange}
