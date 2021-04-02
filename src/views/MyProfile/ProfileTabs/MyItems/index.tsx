0a5b7a4edc5f1f27ba137843400224b66edc9b16
Matt Zamora <mzamora2899@gmail.com>
2021-04-01 22:48:30 -0600

    add more dummy sku tiles

--- src/views/MyProfile/ProfileTabs/MyItems/index.tsx
+++ src/views/MyProfile/ProfileTabs/MyItems/index.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
 import SkuTile from 'components/ProductPanel/SkuTile';
+import styled from 'styled-components/macro';
 
 const MyItems = () => {
   return (
-    <div>
-      <SkuTile status="upcoming" />
-    </div>
+    <MyItemsContainer>
+      <TileContainer>
+        <SkuTile status="upcoming" skuRarity="rare" />
+      </TileContainer>
+      <TileContainer>
+        <SkuTile status="mult-listing" skuRarity="legendary" />
+      </TileContainer>
+      <TileContainer>
+        <SkuTile status="no-sale" skuRarity="uncommon" />
+      </TileContainer>
+      <TileContainer>
+        <SkuTile status="unique" skuRarity="epic" />
+      </TileContainer>
+    </MyItemsContainer>
 
   )
 }
+
+const TileContainer = styled.div`
+  padding: 0 10px;
+`;
+
+const MyItemsContainer = styled.div`
+  display: flex;
+  overflow: auto;
+`;
 
 export default MyItems;
