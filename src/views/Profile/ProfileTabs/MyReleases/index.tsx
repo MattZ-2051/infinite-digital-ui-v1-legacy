7a958f1c6a10344d6ac2f0c0e1f2ead015bc3aa8
Matt Zamora <mzamora2899@gmail.com>
2021-04-04 14:00:02 -0600

    Fix sku padding temporarily

--- src/views/Profile/ProfileTabs/MyReleases/index.tsx
+++ src/views/Profile/ProfileTabs/MyReleases/index.tsx
@@ -4,17 +4,17 @@ import styled from 'styled-components/macro';
 const MyReleases = () => {
   return (
     <MyReleasesContainer>
-      <TileContainer>
+      <TileContainer style={{ paddingRight: '10px', paddingLeft: '0' }}>
         <SkuTile status="upcoming" skuRarity="rare" />
       </TileContainer>
       <TileContainer>
-        <SkuTile status="mult-listing" skuRarity="epic" />
+        <SkuTile status="mult-listing" skuRarity="legendary" />
       </TileContainer>
       <TileContainer>
-        <SkuTile status="no-sale" skuRarity="legendary" />
+        <SkuTile status="no-sale" skuRarity="uncommon" />
       </TileContainer>
-      <TileContainer>
-        <SkuTile status="unique" skuRarity="uncommon" />
+      <TileContainer style={{ paddingRight: '0', paddingLeft: '10px' }}>
+        <SkuTile status="unique" skuRarity="epic" />
       </TileContainer>
     </MyReleasesContainer>
 
