0761a3a4fb43a7c70552f5d5c9906fde2c1fbc35
Matt Zamora <mzamora2899@gmail.com>
2021-04-08 11:30:07 -0600

    add containers to wallet layout

--- src/views/Wallet/index.tsx
+++ src/views/Wallet/index.tsx
@@ -4,12 +4,44 @@ import styled from 'styled-components'
 const Wallet = () => {
 
   return (
-    <Container>My Wallet page</Container>
+    <Container>
+      <PageHeaderContainer>
+        My Wallet
+      </PageHeaderContainer>
+      <PageContentContainer>
+        <TotalBalanceContainer>
+          Total Balance
+        </TotalBalanceContainer>
+        <LatestTransactionsContainer>Latest Transactions</LatestTransactionsContainer>
+      </PageContentContainer>
+    </Container>
   )
 }
 
 const Container = styled.div`
-  height: 100%
+  height: 100vh;
 `;
+
+const PageHeaderContainer = styled.div`
+  background-color: black;
+  height: 30%;
+  color: white;
+`;
+
+const TotalBalanceContainer = styled.div`
+  background-color: #f9f9f9;
+  padding: 48px;
+`;
+
+const LatestTransactionsContainer = styled.div`
+  padding: 48px;
+`;
+
+const PageContentContainer = styled.div`
+  height: 70%;
+  display: grid;
+  grid-template-columns: 30% 70%;
+`;
+
 
 export default Wallet;
