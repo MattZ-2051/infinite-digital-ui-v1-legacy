86a0a07d88e0f9b37776697b410a2c1c104658fb
Matt Zamora <mzamora2899@gmail.com>
2021-04-08 12:21:07 -0600

    add transaction component

---
+++ src/views/Wallet/Transaction/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Transaction = () => {
+
+  return (
+    <div>
+      <div>Icon</div>
+      <span>Money added to your wallet</span>
+      <span>Mar 30, 2021</span>
+      <span>+ $200.00</span>
+    </div>
+  )
+}
+
+export default Transaction;
--- src/views/Wallet/index.tsx
+++ src/views/Wallet/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Transaction from './Transaction';
 
 const Wallet = () => {
 
@@ -10,9 +11,39 @@ const Wallet = () => {
       </PageHeaderContainer>
       <PageContentContainer>
         <TotalBalanceContainer>
-          Total Balance
+          <div>
+            <SectionTitle>Total Balance</SectionTitle>
+            <div style={{ borderBottom: '3px solid #D8D8D8', paddingTop: '10px' }}>
+            </div>
+          </div>
+          <BalanceAmount>$4500</BalanceAmount>
+          <AvailableAmount>
+            <span style={{ fontSize: '16px', fontWeight: 400, color: '#9E9E9E', paddingRight: '8px' }}>
+              Available:
+            </span>
+              $3750 (after active bids)
+          </AvailableAmount>
+          <div style={{ paddingBottom: '12px', paddingTop: '36px' }}>
+            <ActionButton>
+              Deposit
+          </ActionButton>
+          </div>
+          <div style={{ paddingTop: '12px' }}>
+            <ActionButton>
+              Withdrawal
+          </ActionButton>
+          </div>
         </TotalBalanceContainer>
-        <LatestTransactionsContainer>Latest Transactions</LatestTransactionsContainer>
+        <LatestTransactionsContainer>
+          <div>
+            <SectionTitle>Latest Transactions</SectionTitle>
+            <div style={{ borderBottom: '3px solid #D8D8D8', paddingTop: '10px' }}>
+            </div>
+          </div>
+          <Transaction />
+          <Transaction />
+          <Transaction />
+        </LatestTransactionsContainer>
       </PageContentContainer>
     </Container>
   )
@@ -22,6 +53,25 @@ const Container = styled.div`
   height: 100vh;
 `;
 
+const BalanceAmount = styled.span`
+  padding-top: 15px;
+  font-weight: 500;
+  font-size: 48px;
+`;
+
+const AvailableAmount = styled.span`
+  font-size: 16px;
+  font-weight: 400;
+`;
+
+const SectionTitle = styled.span`
+  font-weight: 600;
+  font-size: 22px;
+  line-height: 27.83px;
+  border-bottom: 3px solid black;
+  padding-bottom: 12px;
+`;
+
 const PageHeaderContainer = styled.div`
   background-color: black;
   height: 30%;
@@ -29,8 +79,10 @@ const PageHeaderContainer = styled.div`
 `;
 
 const TotalBalanceContainer = styled.div`
-  background-color: #f9f9f9;
+  background-color: #F4F4F4;
   padding: 48px;
+  display: flex;
+  flex-direction: column;
 `;
 
 const LatestTransactionsContainer = styled.div`
@@ -43,5 +95,21 @@ const PageContentContainer = styled.div`
   grid-template-columns: 30% 70%;
 `;
 
+const ActionButton = styled.button`
+  width: 269px;
+  height 56px;
+  color: black;
+  background-color: white;
+  border: 2px solid black;
+  font-weight: 600;
+  font-size: 20px;
+  border-radius: 35px;
+  :hover {
+    background-color: black;
+    color: white;
+    cursor: pointer;
+  }
+
+`;
 
 export default Wallet;
