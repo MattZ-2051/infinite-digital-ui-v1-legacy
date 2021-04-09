949539a71e9e06193cc61876008d7816c9f66a45
Matt Zamora <mzamora2899@gmail.com>
2021-04-08 20:27:45 -0600

    fix bottom border styles

--- src/views/Wallet/index.tsx
+++ src/views/Wallet/index.tsx
@@ -18,10 +18,10 @@ const Wallet = () => {
       <PageContentContainer>
         <TotalBalanceContainer>
           <div >
-            <Tab style={{ borderBottom: '3px solid black' }}>
+            <Tab style={{ borderBottom: '2px solid black' }}>
               Total Balance
             </Tab>
-            <div style={{ borderBottom: '3px solid #D8D8D8', paddingTop: '10px', width: '80%' }}></div>
+            <div style={{ borderBottom: '2px solid #D8D8D8', paddingTop: '10px', width: '80%' }}></div>
           </div>
           <BalanceAmount>$4500</BalanceAmount>
           <AvailableAmount>
@@ -44,19 +44,19 @@ const Wallet = () => {
         <LatestTransactionsContainer>
           <div style={{ position: 'relative' }}>
             <Tab
-              style={{ borderBottom: `${selectedTab === 0 ? '3px solid black' : 'none'}`, color: `${selectedTab === 0 ? 'black' : '#9e9e9e'}` }}
+              style={{ borderBottom: `${selectedTab === 0 ? '2px solid black' : 'none'}`, color: `${selectedTab === 0 ? 'black' : '#9e9e9e'}` }}
               onClick={() => setSelectedTab(0)}
             >
               Latest Transactions
               </Tab>
             <span style={{ padding: '0 20px' }}></span>
             <Tab
-              style={{ borderBottom: `${selectedTab === 1 ? '3px solid black' : 'none'}`, color: `${selectedTab === 1 ? 'black' : '#9e9e9e'}` }}
+              style={{ borderBottom: `${selectedTab === 1 ? '2px solid black' : 'none'}`, color: `${selectedTab === 1 ? 'black' : '#9e9e9e'}` }}
               onClick={() => setSelectedTab(1)}
             >
               Active Bids
               </Tab>
-            <div style={{ borderBottom: '3px solid #D8D8D8', width: '100%', paddingTop: '10px' }}></div>
+            <div style={{ borderBottom: '2px solid #D8D8D8', width: '100%', paddingTop: '10px' }}></div>
           </div>
           <Transaction />
           <Transaction />
