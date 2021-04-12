31abca04b8de63a45ad2873b2a3ae390929e44a1
Matt Zamora <mzamora2899@gmail.com>
2021-04-12 11:04:56 -0600

    add props to transaction component

--- src/views/Wallet/Transaction/index.tsx
+++ src/views/Wallet/Transaction/index.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Transaction = () => {
+interface IProps {
+  date?: string;
+  transactionDetail?: string;
+  amount?: string;
+  transactionType?: 'cc-deposit' | 'coinbase' | 'withdraw' | 'sale' | 'purchase'
+}
+
+const Transaction = ({ date, transactionDetail, amount, transactionType }: IProps) => {
 
   return (
     <Container>
       <div>Icon</div>
-      <TransactionDescription><span style={{ color: 'black' }}>Money added</span> to your wallet</TransactionDescription>
-      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-        <span>Mar 30, 2021</span>
-      </div>
-      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-        <span>+ $200.00</span>
-      </div>
+      <TransactionDescription>
+        <span style={{ color: 'black' }}>Money added</span> to your wallet
+        </TransactionDescription>
+      <TransactionDetail>
+        <span>{date || 'Mar 30, 2021'}</span>
+      </TransactionDetail>
+      <TransactionDetail>
+        <span>+ {amount || '$200.00'}</span>
+      </TransactionDetail>
     </Container>
   )
 }
@@ -23,6 +32,11 @@ const Container = styled.div`
   border-top: 1px solid #EBEBEB;
   border-bottom: 1px solid #EBEBEB;
   padding: 10px 0;
+`;
+
+const TransactionDetail = styled.div`
+  display: flex;
+  justify-content: flex-end;
 `;
 
 const TransactionDescription = styled.span`
