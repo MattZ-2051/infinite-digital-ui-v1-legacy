a4a33d8ad4d9a87b06ced5324f7d7739963f9d3b
Matt Zamora <mzamora2899@gmail.com>
2021-04-13 22:17:12 -0600

    add modal component and layout

--- src/views/Wallet/index.tsx
+++ src/views/Wallet/index.tsx
@@ -2,10 +2,96 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import styled from 'styled-components'
 import Transaction from './Transaction';
+import Modal from '@material-ui/core/Modal';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import coinbaseIcon from 'assets/img/icons/coinbase.png'
+import sukuIcon from 'assets/img/icons/suku-icon.png'
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    paper: {
+      position: 'absolute',
+      width: '565px',
+      height: '511px',
+      backgroundColor: theme.palette.background.paper,
+      boxShadow: theme.shadows[5],
+      padding: '48px',
+      outline: 'none',
+      borderRadius: '10px'
+    },
+  }),
+);
+
+
 
 const Wallet = () => {
 
   const [selectedTab, setSelectedTab] = useState<number | undefined>(0)
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const handleClose = () => {
+    setIsModalOpen(false)
+  }
+
+  const handleOpen = () => {
+    setIsModalOpen(true)
+  }
+
+  const classes = useStyles();
+  // getModalStyle is not a pure function, we roll the style only on the first render
+  const [modalStyle] = React.useState(getModalStyle);
+
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <span style={{ fontSize: '22px', fontWeight: 600, borderBottom: '2px solid black', paddingBottom: '14px' }}>Select a payment to deposit</span>
+      <GrayLine style={{ width: '100%' }}></GrayLine>
+      <div style={{ fontSize: '16px', color: '#7d7d7d', paddingTop: '25px' }}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </div>
+      <div style={{ display: 'grid', gridTemplateColumns: '15% 55% 30%', paddingTop: '40px', borderBottom: '1px solid #EBEBEB', paddingBottom: '21px' }}>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <img src={coinbaseIcon} />
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <span style={{ fontSize: '16px', fontWeight: 600 }}>Circle</span>
+          <span style={{ fontSize: '16px', color: '#9e9e9e' }}>Pay with credit card</span>
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}><ArrowForwardIosIcon /></div>
+      </div>
+      <div style={{ display: 'grid', gridTemplateColumns: '15% 55% 30%', paddingTop: '40px', borderBottom: '1px solid #EBEBEB', paddingBottom: '21px' }}>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <img src={coinbaseIcon} />
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <span style={{ fontSize: '16px', fontWeight: 600 }}>Coinbase</span>
+          <span style={{ fontSize: '16px', color: '#9e9e9e' }}>Pay with cryptocurrency</span>
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}><ArrowForwardIosIcon /></div>
+      </div>
+      <div style={{ display: 'grid', gridTemplateColumns: '15% 55% 30%', paddingTop: '40px', borderBottom: '1px solid #EBEBEB', paddingBottom: '21px' }}>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <img src={sukuIcon} />
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <span style={{ fontSize: '16px', fontWeight: 600, color: '#9e9e9e' }}>Suku</span>
+          <span style={{ fontSize: '16px', color: '#9e9e9e' }}>Coming soon</span>
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}><ArrowForwardIosIcon style={{ color: '#9e9e9e' }} /></div>
+      </div>
+    </div>
+  );
 
   return (
     <Container>
@@ -31,7 +117,7 @@ const Wallet = () => {
               $3750 (after active bids)
           </AvailableAmount>
           <div style={{ paddingBottom: '12px', paddingTop: '36px' }}>
-            <ActionButton>
+            <ActionButton onClick={handleOpen}>
               Deposit
           </ActionButton>
           </div>
@@ -65,7 +151,9 @@ const Wallet = () => {
               <Transaction transactionType='purchase' />
             </>
           )}
-
+          <Modal open={isModalOpen} onClose={handleClose} aria-labelledby="simple-modal-title" aria-describedby="simple-modal-description">
+            {body}
+          </Modal>
         </LatestTransactionsContainer>
       </PageContentContainer>
     </Container>
@@ -148,6 +236,9 @@ const ActionButton = styled.button`
     background-color: black;
     color: white;
     cursor: pointer;
+  }
+  :focus {
+    outline: none;
   }
 `;
 
