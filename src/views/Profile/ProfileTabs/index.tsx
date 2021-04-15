b53b93facaa4e68f71fdf503cd1630dcd2820cad
Matt Zamora <mzamora2899@gmail.com>
2021-04-15 17:08:16 -0600

    fix styles

--- src/views/Profile/ProfileTabs/index.tsx
+++ src/views/Profile/ProfileTabs/index.tsx
@@ -1,91 +1,140 @@
-import { useState } from 'react'
-import styled from 'styled-components';
-import MyItems from './MyItems';
-import MyReleases from './MyReleases';
+import { useState } from "react";
+import styled from "styled-components";
+import MyItems from "./MyItems";
+import MyReleases from "./MyReleases";
+import SortByFilter from "./SortByFilter";
 
 interface IProps {
-  userStatus?: 'loggedInIssuer' | 'loggedIn' | 'notCurrentUserProfile' | 'notCurrentUserProfileIssuer';
+  userStatus?:
+    | "loggedInIssuer"
+    | "loggedIn"
+    | "notCurrentUserProfile"
+    | "notCurrentUserProfileIssuer";
 }
 
 const ProfileTabs = ({ userStatus }: IProps) => {
+  const [selectedTab, setSelectedTab] = useState<number | undefined>(0);
 
-  const [selectedTab, setSelectedTab] = useState<number | undefined>(0)
-
+  const placeHolderFunc = () => {};
   return (
-    <Container >
-      {userStatus === 'loggedIn' && (
+    <Container>
+      {userStatus === "loggedIn" && (
         <>
-          <div style={{ position: 'relative', paddingBottom: '30px' }}>
+          <div style={{ position: "relative", paddingBottom: "30px" }}>
             <Tab
-              style={{ borderBottom: `${selectedTab === 0 ? '2px solid black' : 'none'}`, color: `${selectedTab === 0 ? 'black' : '#9e9e9e'}` }}
-              onClick={() => setSelectedTab(0)}>
+              style={{
+                borderBottom: `${
+                  selectedTab === 0 ? "2px solid black" : "none"
+                }`,
+                color: `${selectedTab === 0 ? "black" : "#9e9e9e"}`,
+              }}
+              onClick={() => setSelectedTab(0)}
+            >
               My Items
-              </Tab>
-            <span style={{ padding: '0 20px' }}></span>
-            <GrayLine style={{ width: '100%' }}></GrayLine>
+            </Tab>
+            <span style={{ padding: "0 20px" }}></span>
+            <GrayLine style={{ width: "100%" }}></GrayLine>
           </div>
 
           {selectedTab === 0 && <MyItems />}
-
         </>
       )}
-      {userStatus === 'loggedInIssuer' && (
+      {userStatus === "loggedInIssuer" && (
         <>
-          <div style={{ position: 'relative', paddingBottom: '30px' }}>
+          <div style={{ position: "relative", paddingBottom: "30px" }}>
             <Tab
-              style={{ borderBottom: `${selectedTab === 0 ? '2px solid black' : 'none'}`, color: `${selectedTab === 0 ? 'black' : '#9e9e9e'}` }}
-              onClick={() => setSelectedTab(0)}>
+              style={{
+                borderBottom: `${
+                  selectedTab === 0 ? "2px solid black" : "none"
+                }`,
+                color: `${selectedTab === 0 ? "black" : "#9e9e9e"}`,
+              }}
+              onClick={() => setSelectedTab(0)}
+            >
               My Releases
-              </Tab>
-            <span style={{ padding: '0 20px' }}></span>
+            </Tab>
+            <span style={{ padding: "0 20px" }}></span>
             <Tab
-              style={{ borderBottom: `${selectedTab === 1 ? '2px solid black' : 'none'}`, color: `${selectedTab === 1 ? 'black' : '#9e9e9e'}` }}
-              onClick={() => setSelectedTab(1)}>
+              style={{
+                borderBottom: `${
+                  selectedTab === 1 ? "2px solid black" : "none"
+                }`,
+                color: `${selectedTab === 1 ? "black" : "#9e9e9e"}`,
+              }}
+              onClick={() => setSelectedTab(1)}
+            >
               My Items
-              </Tab>
-            <GrayLine style={{ width: '100%' }}></GrayLine>
+            </Tab>
+            <GrayLine style={{ width: "100%" }}></GrayLine>
           </div>
           {selectedTab === 0 && <MyReleases />}
           {selectedTab === 1 && <MyItems />}
         </>
       )}
-      {userStatus === 'notCurrentUserProfileIssuer' && (
+      {userStatus === "notCurrentUserProfileIssuer" && (
         <>
-          <div style={{ position: 'relative', paddingBottom: '30px' }}>
-            <Tab
-              style={{ borderBottom: `${selectedTab === 0 ? '2px solid black' : 'none'}`, color: `${selectedTab === 0 ? 'black' : '#9e9e9e'}` }}
-              onClick={() => setSelectedTab(0)}>
-              Latest Transactions
-              </Tab>
-            <span style={{ padding: '0 20px' }}></span>
-            <Tab
-              style={{ borderBottom: `${selectedTab === 1 ? '2px solid black' : 'none'}`, color: `${selectedTab === 1 ? 'black' : '#9e9e9e'}` }}
-              onClick={() => setSelectedTab(1)}>
-              Active Bids
-              </Tab>
-            <GrayLine style={{ width: '100%' }}></GrayLine>
+          <div style={{ position: "relative", paddingBottom: "30px" }}>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <div>
+                <Tab
+                  style={{
+                    borderBottom: `${
+                      selectedTab === 0 ? "2px solid black" : "none"
+                    }`,
+                    color: `${selectedTab === 0 ? "black" : "#9e9e9e"}`,
+                  }}
+                  onClick={() => setSelectedTab(0)}
+                >
+                  Releases
+                </Tab>
+                <span style={{ padding: "0 20px" }}></span>
+                <Tab
+                  style={{
+                    borderBottom: `${
+                      selectedTab === 1 ? "2px solid black" : "none"
+                    }`,
+                    color: `${selectedTab === 1 ? "black" : "#9e9e9e"}`,
+                  }}
+                  onClick={() => setSelectedTab(1)}
+                >
+                  Items
+                </Tab>
+              </div>
+              <SortByFilter
+                options={["Latest"]}
+                handleFilter={placeHolderFunc}
+                activeFilterSort={""}
+              />
+            </div>
+            <GrayLine style={{ width: "100%" }}></GrayLine>
           </div>
           {selectedTab === 0 && <MyReleases />}
           {selectedTab === 1 && <MyItems />}
         </>
       )}
-      {userStatus === 'notCurrentUserProfile' && (
+      {userStatus === "notCurrentUserProfile" && (
         <>
-          <div style={{ position: 'relative', paddingBottom: '30px' }}>
+          <div style={{ position: "relative", paddingBottom: "30px" }}>
             <Tab
-              style={{ borderBottom: `${selectedTab === 0 ? '2px solid black' : 'none'}`, color: `${selectedTab === 0 ? 'black' : '#9e9e9e'}` }}
-              onClick={() => setSelectedTab(0)}>
+              style={{
+                borderBottom: `${
+                  selectedTab === 0 ? "2px solid black" : "none"
+                }`,
+                color: `${selectedTab === 0 ? "black" : "#9e9e9e"}`,
+              }}
+              onClick={() => setSelectedTab(0)}
+            >
               Items
-              </Tab>
-            <span style={{ padding: '0 20px' }}></span>
-            <GrayLine style={{ width: '100%' }}></GrayLine>
+            </Tab>
+            <span style={{ padding: "0 20px" }}></span>
+            <GrayLine style={{ width: "100%" }}></GrayLine>
           </div>
           {selectedTab === 0 && <MyItems />}
         </>
       )}
     </Container>
-  )
-}
+  );
+};
 
 const Container = styled.div`
   width: 100%;
@@ -95,23 +144,22 @@ const Container = styled.div`
 
 const GrayLine = styled.div`
   border-bottom: 2px solid #d8d8d8;
-  padding-top: 10px;
-  width: 80%
+  width: 80%;
 `;
 
 const Tab = styled.span`
   font-weight: 600;
   font-size: 22px;
   line-height: 27.83px;
-  padding-bottom: 12px;
+  padding-bottom: 15px;
   border: none;
   position: relative;
   :hover {
     cursor: pointer;
   }
   :focus {
-    outline: none
+    outline: none;
   }
 `;
 
-export default ProfileTabs
+export default ProfileTabs;
