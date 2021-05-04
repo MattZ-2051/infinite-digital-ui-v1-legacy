e05de90906e67900c5e098b41c3d462b3bd8fe6f
JoaquinBeceiro <joaquinbeceiro@gmail.com>
2021-05-04 09:48:01 -0300

    Use utils Toast

--- src/views/Wallet/CCDeposit/AddFunds/index.tsx
+++ src/views/Wallet/CCDeposit/AddFunds/index.tsx
@@ -14,7 +14,7 @@ import {
   removeUserCCThunk,
   addFundsThunk,
 } from 'store/session/sessionThunks';
-import Toast from 'components/Toast';
+import Toast from 'utils/Toast';
 
 const S: any = {};
 
@@ -25,11 +25,6 @@ const AddFunds = () => {
   const dispatch = useAppDispatch();
   const { getAccessTokenSilently } = useAuth0();
   const [amount, setAmount] = useState<string | undefined>('');
-  const [isToastVisible, setIsToastVisible] = useState<boolean>(false);
-  const [toastStatus, setToastStatus] = useState<'error' | 'success'>(
-    'success'
-  );
-  const [toastMessage, setToastMessage] = useState<string>('');
   const fundsBody = {
     email: userCard.metadata.email,
     amount: amount,
@@ -51,7 +46,7 @@ const AddFunds = () => {
     const userToken = await getAccessTokenSilently();
     fundsBody.amount = fundsBody.amount?.replace(',', '');
     if (isNaN(Number(fundsBody?.amount))) {
-      displayToast('error', 'An Error Occurred: Please enter a valid amount.');
+      Toast.error('An Error Occurred: Please enter a valid amount.');
       return;
     }
     const res = await dispatch(
@@ -72,10 +67,10 @@ const AddFunds = () => {
     );
 
     if (res.type.split('/')[5] === 'rejected') {
-      displayToast('error', 'An Error Occurred');
+      Toast.error('An Error Occurred');
       return;
     } else {
-      displayToast('success', 'Card Successfully Removed');
+      Toast.success('Card Successfully Removed');
 
       setTimeout(() => {
         history.push(`/wallet/${username}/addcreditcard`);
@@ -83,90 +78,73 @@ const AddFunds = () => {
     }
   };
 
-  function displayToast(type: 'success' | 'error', msg: string) {
-    setIsToastVisible(true);
-    setToastStatus(type);
-    setToastMessage(msg);
-  }
-
   const year = userCard.expYear.toString().slice(2, 4);
   const month = userCard.expMonth.toString();
 
   const expDate = (month.length === 1 ? '0' + month : month) + '/' + year;
 
   return (
-    <>
-      <Toast
-        isVisible={isToastVisible}
-        status={toastStatus}
-        setIsVisible={setIsToastVisible}
-      >
-        {toastMessage}
-      </Toast>
-      <Container>
-        <S.ContentContainer>
-          <S.Row
-            style={{ borderBottom: '2px solid black', paddingBottom: '16px' }}
-          >
-            <S.HeaderDiv>
-              <img src={circleIcon} alt="" />
-              <S.HeaderText>Circle Payments</S.HeaderText>
-            </S.HeaderDiv>
-          </S.Row>
-          <div style={{ paddingTop: '25px' }}>
-            <S.AddFundsText>Add funds into your wallet</S.AddFundsText>
+    <Container>
+      <S.ContentContainer>
+        <S.Row
+          style={{ borderBottom: '2px solid black', paddingBottom: '16px' }}
+        >
+          <S.HeaderDiv>
+            <img src={circleIcon} alt="" />
+            <S.HeaderText>Circle Payments</S.HeaderText>
+          </S.HeaderDiv>
+        </S.Row>
+        <div style={{ paddingTop: '25px' }}>
+          <S.AddFundsText>Add funds into your wallet</S.AddFundsText>
+        </div>
+        <S.CardContainer>
+          <S.CreditCard
+            name=" "
+            expiry={expDate}
+            focus=""
+            number={`************${userCard.last4}`}
+            preview={true}
+            issuer={`${userCard.network}`}
+          />
+        </S.CardContainer>
+        <S.Row>
+          <div>
+            <span>Credit Card</span>
+            <span style={{ color: '#00c44f', paddingLeft: '5px' }}>
+              (Active)
+            </span>
           </div>
-          <S.CardContainer>
-            <S.CreditCard
-              name=" "
-              expiry={expDate}
-              focus=""
-              number={`************${userCard.last4}`}
-              preview={true}
-              issuer={`${userCard.network}`}
-            />
-          </S.CardContainer>
-          <S.Row>
-            <div>
-              <span>Credit Card</span>
-              <span style={{ color: '#00c44f', paddingLeft: '5px' }}>
-                (Active)
-              </span>
-            </div>
-            <S.RemoveCCButton onClick={removeCard}>
-              Remove Card
-            </S.RemoveCCButton>
-          </S.Row>
-          <div
-            style={{
-              paddingTop: '25px',
-              borderBottom: '2px solid #ebebeb',
-              paddingBottom: '10px',
-            }}
-          >
-            <S.DollarSign>$</S.DollarSign>
-            {/* <S.AmountInput
+          <S.RemoveCCButton onClick={removeCard}>Remove Card</S.RemoveCCButton>
+        </S.Row>
+        <div
+          style={{
+            paddingTop: '25px',
+            borderBottom: '2px solid #ebebeb',
+            paddingBottom: '10px',
+          }}
+        >
+          <S.DollarSign>$</S.DollarSign>
+          {/* <S.AmountInput
             placeholder="Enter Amount"
             onChange={(e) => setAmount(e.target.value)}
           /> */}
-            <S.AmountInput
-              id="amount"
-              name="amount-input"
-              placeholder="Enter Amount"
-              decimalsLimit={2}
-              onChange={handleChange}
-              maxLength={10}
-              step={10}
-              defaultValue={0.0}
-              allowNegativeValue={false}
-            />
-          </div>
-          <div style={{ padding: '25px 0' }}>
-            <S.AddFundsButton onClick={addFunds}>Add Funds</S.AddFundsButton>
-          </div>
-        </S.ContentContainer>
-      </Container>
-    </>
+          <S.AmountInput
+            id="amount"
+            name="amount-input"
+            placeholder="Enter Amount"
+            decimalsLimit={2}
+            onChange={handleChange}
+            maxLength={10}
+            step={10}
+            defaultValue={0.0}
+            allowNegativeValue={false}
+          />
+        </div>
+        <div style={{ padding: '25px 0' }}>
+          <S.AddFundsButton onClick={addFunds}>Add Funds</S.AddFundsButton>
+        </div>
+      </S.ContentContainer>
+    </Container>
   );
 };
 
