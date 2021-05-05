03a153d11d9ebccd2e1784102557a3e53eaf1e04
Matt Zamora <mzamora2899@gmail.com>
2021-05-05 09:45:03 -0600

    fix styles

--- src/views/Collection/UserCollectionTabs/index.tsx
+++ src/views/Collection/UserCollectionTabs/index.tsx
@@ -246,7 +246,7 @@ const UserCollectionTabs = ({ user, isAuthenticated }: IProps) => {
 const Container = styled.div`
   width: 100%;
   padding: 40px;
-  height: 70vh;
+  height: 100vh;
 `;
 
 const GrayLine = styled.div`
