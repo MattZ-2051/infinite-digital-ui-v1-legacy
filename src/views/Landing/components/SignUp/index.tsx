cbe4919f90c52a622873dec8429ef71468dd92b3
Matt Zamora <mzamora2899@gmail.com>
2021-05-05 00:18:46 -0600

    add aria landing sign up

--- src/views/Landing/components/SignUp/index.tsx
+++ src/views/Landing/components/SignUp/index.tsx
@@ -1,7 +1,15 @@
 import { S } from './styles';
 
 const SignUp = () => {
-  return <S.Container>TRAVIS SCOTT SIGNUP</S.Container>;
+  return (
+    <S.Container>
+      <S.Header>{"Travis Scott's Coming in Hot!"}</S.Header>
+      <S.SubHeader>SIgn up now, never miss an NFT drop.</S.SubHeader>
+      <S.FlexCenter>
+        <S.Button>Sign up now</S.Button>
+      </S.FlexCenter>
+    </S.Container>
+  );
 };
 
 export default SignUp;
