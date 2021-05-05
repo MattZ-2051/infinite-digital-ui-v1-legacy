0cf1fc3d88084aa26a524da522277ea82a1ac065
brycedoganer <bryce.doganer@gmail.com>
2021-05-04 22:25:52 -0400

    feat: Add collection view

---
+++ src/store/user/userFactory.ts
@@ -0,0 +1,25 @@
+import * as Factory from 'factory.ts';
+import { User } from 'entities/user';
+
+export const userFactory = Factory.Sync.makeFactory<User>({
+  _id: '0',
+  id: '0',
+  balance: 0,
+  availableBalance: 0,
+  createdAt: new Date(),
+  email: '',
+  externalId: '0',
+  hederaAccount: '0',
+  role: 'none',
+  updatedAt: '',
+  username: '',
+  profilePhotoUrl: '',
+  bannerPhotoUrl: '',
+  midPhotoUrl: '',
+  descriptionIcon: '',
+  descriptionHeader: '',
+  descriptionBody: '',
+  footerPhotoUrl: '',
+  tagline: '',
+  transactions: [],
+});
--- src/views/Collection/index.tsx
+++ src/views/Collection/index.tsx
@@ -5,17 +5,58 @@ import UserCollectionTabs from './UserCollectionTabs';
 import { useHistory } from 'react-router-dom';
 import { getUser } from 'services/api/userService';
 import { useAuth0 } from '@auth0/auth0-react';
+import { FlexColumn, FlexRow } from 'components/Layout';
+import { Image, BackgroundImageContainer } from 'components/Image';
+import { TextContainer, GradientText } from '../../components/Text';
+import { User } from 'entities/user';
+import { userFactory } from 'store/user/userFactory';
 
-const Collection = () => {
-  const [user, setUser] = useState<any>(null);
+const splitLastSentence = (text: string): [string, string] => {
+  const splitText = text.split('.');
+
+  let lastSentence = splitText?.pop() + '.';
+  if (lastSentence === '.') {
+    lastSentence = splitText.pop() + '.';
+  }
+
+  if (lastSentence === '".') {
+    lastSentence = splitText.pop() + '."';
+  }
+
+  const startText =
+    splitText?.length &&
+    splitText?.reduce((text, currentText) => (text = text + currentText + '.'));
+
+  return [startText || '', lastSentence || ''];
+};
+
+const Collection = (): JSX.Element => {
+  const [user, setUser] = useState<User>(userFactory.build());
+  const {
+    profilePhotoUrl,
+    bannerPhotoUrl,
+    midPhotoUrl,
+    descriptionIcon,
+    descriptionHeader,
+    descriptionBody,
+    footerPhotoUrl,
+    tagline,
+  } = user;
+  const [descriptionHeaderMain, descriptionHeaderGradient] = splitLastSentence(
+    descriptionHeader
+  );
+  const [taglineMain, taglineGradient] = splitLastSentence(tagline);
+
   const history = useHistory();
   const userId = history.location.pathname.split('/')[2];
   const { isAuthenticated } = useAuth0();
 
   async function fetchUser() {
     try {
-      const res = await getUser(userId);
-      setUser(res);
+      const data = await getUser(userId);
+      if (data) {
+        setUser(data);
+      }
     } catch (e) {
       console.log(e);
     }
@@ -29,12 +70,62 @@ const Collection = () => {
 
   return (
     <Container>
-      <UserCollectionInfo user={user} isAuthenticated={isAuthenticated} />
+      <BackgroundImageContainer
+        src={bannerPhotoUrl}
+        height="500px"
+        width="1400px"
+      >
+        <UserCollectionInfo user={user} isAuthenticated={isAuthenticated} />
+      </BackgroundImageContainer>
+      <FlexRow style={{ margin: '5rem' }}>
+        <Container style={{ marginRight: '2.5rem' }}>
+          <Image src={midPhotoUrl} height="648px" width="518px" />
+        </Container>
+        <Container style={{ marginLeft: '2.5rem' }}>
+          <FlexColumn childMargin="1rem" style={{ margin: '1rem' }}>
+            <Image src={descriptionIcon} height="98px" width="98px" />
+            <TextContainer textAlign="left" fontSize="28">
+              {descriptionHeaderMain}
+              <GradientText textAlign="left" fontSize="28">
+                {descriptionHeaderGradient}
+              </GradientText>
+            </TextContainer>
+            <TextContainer fontSize="18">{descriptionBody}</TextContainer>
+          </FlexColumn>
+        </Container>
+      </FlexRow>
       <UserCollectionTabs user={user} isAuthenticated={isAuthenticated} />
+      <Container
+        style={{
+          marginLeft: '2.5rem',
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <FlexColumn
+          childMargin="1rem"
+          style={{ margin: '1rem', alignItems: 'center', width: '80%' }}
+        >
+          <Image src={footerPhotoUrl} height="600px" width="500px" />
+          <TextContainer textAlign="center" fontSize="48" fontWeight="400">
+            {taglineMain}
+            <GradientText textAlign="center" fontSize="48" fontWeight="400">
+              {taglineGradient}
+            </GradientText>
+          </TextContainer>
+        </FlexColumn>
+      </Container>
     </Container>
   );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  background-color: black;
+  color: white;
 
+  > * {
+    background-color: black;
+    color: white;
+  }
+`;
 export default Collection;
