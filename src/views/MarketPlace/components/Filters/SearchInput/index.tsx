b8603beb3b4c4bd158334480cb316c7214f5f010
pnoguera <pnoguera.dev@gmail.com>
2021-05-13 10:38:33 -0300

    Change search input placeholder - marketplace

--- src/views/MarketPlace/components/Filters/SearchInput/index.tsx
+++ src/views/MarketPlace/components/Filters/SearchInput/index.tsx
@@ -44,7 +44,7 @@ const SearchInput = ({ handleFilter, activeFilters }: IProps) => {
           name="search"
           value={searchValue}
           type="text"
-          placeholder="Search by name"
+          placeholder="Search"
         />
       </InputDiv>
     </>
