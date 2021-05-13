6dd642af2e7cffc11d9b946c3e322298e2b47071
Morfo Fragkopoulou <mfragkop@gmail.com>
2021-05-13 19:20:27 +0300

    bug/help-376 -> development: Changes in help page (#348)
    * bug/help-376: Changes in help page
    
    * fix help page styels
    
    Co-authored-by: Morfo <morfo.frankopoulou@cognitivplus.com>
    Co-authored-by: Matt Zamora <mzamora2899@gmail.com>

--- src/views/Legal/help.tsx
+++ src/views/Legal/help.tsx
@@ -6,7 +6,7 @@ interface WindowWithHubspotForm extends Window {
   readonly hbspt?: any;
 }
 
-const Help: React.FC<unknown> = () => {
+const Help = () => {
   const [loaded, error] = useScript('//js.hsforms.net/forms/v2.js');
 
   useEffect(() => {
@@ -16,13 +16,37 @@ const Help: React.FC<unknown> = () => {
         windowWithHubspotForm.hbspt.forms.create({
           region: 'na1',
           portalId: '8953348',
-          formId: '22f866de-9203-45d5-83c6-75a4a1f53ae6',
+          formId: 'a25ae540-4e5a-4858-90fb-20edc5ca1252',
           target: '#embed-hubspot',
         });
       }
     }
   }, [loaded, error]);
-  return <S.Container id="embed-hubspot"></S.Container>;
+  return (
+    <S.HelpContainer>
+      <S.HelpColumn>
+        <S.Header>How can we help you?</S.Header>
+        <S.SubTitle>
+          Before submitting this support form, please search the{' '}
+          <a
+            target="_blank"
+            href="https://support.suku.world/"
+            rel="noreferrer"
+          >
+            SUKU Knowledge Base
+          </a>
+          for information on common support questions.
+        </S.SubTitle>
+        <S.SubTitle>
+          Want to learn more, make suggestions, or just need help? Leave us a
+          message and we&apos;ll get back to you!
+        </S.SubTitle>
+      </S.HelpColumn>
+      <S.HelpColumn>
+        <S.Container id="embed-hubspot"></S.Container>
+      </S.HelpColumn>
+    </S.HelpContainer>
+  );
 };
 
 export default Help;
