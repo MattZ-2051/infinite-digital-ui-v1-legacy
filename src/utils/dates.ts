57361dec58f394d7ab1ace0bcedf998074185901
Lucas H <lhenning@citizensreserve.com>
2021-05-18 09:53:02 -0400

    added seconds to the button block countdown (#401)

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -8,16 +8,19 @@ export const formatCountdown = (date: Date): string => {
     const now = DateTime.now();
 
     const diff = startDayInLocal
-      .diff(now, ['years', 'months', 'days', 'hours', 'minutes'])
+      .diff(now, ['years', 'months', 'days', 'hours', 'minutes', 'seconds'])
       .toObject();
 
     const monthsLeft = diff.months as number;
     const daysLeft = diff.days as number;
     const hoursLeft = diff.hours as number;
     const minutesLeft = Math.round(diff.minutes) as number;
+    const secondsLeft = Math.round(diff.seconds) as number;
 
-    if (daysLeft === 0) return `${hoursLeft}h ${Math.round(minutesLeft)}m`;
-    if (daysLeft < 28) return `${daysLeft}d ${hoursLeft}h ${minutesLeft}m`;
+    if (daysLeft === 0)
+      return `${hoursLeft}h ${Math.round(minutesLeft)}m ${secondsLeft}s`;
+    if (daysLeft < 28)
+      return `${daysLeft}d ${hoursLeft}h ${minutesLeft}m ${secondsLeft}s`;
     if (monthsLeft === 0) return `${daysLeft}d`;
     return `${monthsLeft}m ${daysLeft}d`;
   } catch (e) {
