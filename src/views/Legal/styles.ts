b1582b2a619265bb7896d6eab1e72bb079cf9d3d
Morfo Fragkopoulou <mfragkop@gmail.com>
2021-05-19 16:05:38 +0300

    feature/265: update terms and conditions (#426)
    Co-authored-by: Morfo <morfo.frankopoulou@cognitivplus.com>

--- src/views/Legal/styles.ts
+++ src/views/Legal/styles.ts
@@ -39,3 +39,263 @@ export const SubTitle = styled.div`
     margin-right: 5px;
   }
 `;
+
+export const P1 = styled.p`
+  margin: 0px 0px 12px 0px;
+  text-align: center;
+  font: 12px 'Times New Roman';
+  color: #000000;
+`;
+
+export const P2 = styled.p`
+  margin: 0px 0px 12px 0px;
+  text-align: justify;
+  font: 12px Times;
+  color: #000000;
+`;
+
+export const P3 = styled.p`
+  margin: 0px 0px 12px 0px;
+  text-align: justify;
+  text-indent: 36px;
+  font: 12px 'Times New Roman';
+  color: #000000;
+`;
+
+export const P4 = styled.p`
+  margin: 0px 0px 12px 0px;
+  font: 12px 'Times New Roman';
+  color: #000000;
+`;
+
+export const P5 = styled.p`
+  margin: 0px 0px 12px 0px;
+  text-align: justify;
+  text-indent: 36px;
+  font: 12px 'Times New Roman';
+  color: #0d0e0e;
+  background-color: #ffffff;
+`;
+
+export const P6 = styled.p`
+  margin: 0px 0px 12px 0px;
+  text-align: justify;
+  text-indent: 36px;
+  font: 12px 'Times New Roman';
+  color: #0d0e0e;
+`;
+
+export const P7 = styled.p`
+  margin: 0px 0px 6px 0px;
+  text-align: justify;
+  text-indent: 36px;
+  font: 12px 'Times New Roman';
+  color: #0d0e0e;
+  background-color: #ffffff;
+`;
+
+export const P8 = styled.p`
+  margin: 0px 0px 6px 0px;
+  text-align: justify;
+  text-indent: 36px;
+  font: 12px 'Times New Roman';
+  color: #000000;
+`;
+
+export const P9 = styled.p`
+  margin: 0px 0px 0px 0px;
+  text-align: justify;
+  font: 12px Times;
+  color: #000000;
+`;
+
+export const P10 = styled.p`
+  margin: 0px 0px 0px 0px;
+  text-align: justify;
+  font: 12px Times;
+  color: #000000;
+  min-height: 14px;
+`;
+
+export const P11 = styled.p`
+  margin: 0px 0px 0px 0px;
+  text-align: justify;
+  font: 12px 'Times New Roman';
+  color: #000000;
+`;
+
+export const P12 = styled.p`
+  margin: 0px 0px 0px 0px;
+  text-align: justify;
+  font: 12px 'Times New Roman';
+  color: #000000;
+  min-height: 15px;
+`;
+
+export const P13 = styled.p`
+  margin: 0px 0px 0px 0px;
+  text-align: justify;
+  text-indent: 36px;
+  font: 12px 'Times New Roman';
+  color: #000000;
+`;
+
+export const P14 = styled.p`
+  margin: 0px 0px 0px 0px;
+  text-align: justify;
+  text-indent: 36px;
+  font: 12px 'Times New Roman';
+  color: #000000;
+  min-height: 15px;
+`;
+
+export const P15 = styled.p`
+  margin: 0px 0px 0px 0px;
+  text-align: justify;
+  font: 12px 'Times New Roman';
+  color: #000000;
+  min-height: 15px;
+`;
+
+export const P16 = styled.p`
+  margin: 0px 0px 12px 0px;
+  text-align: justify;
+  text-indent: 36px;
+  font: 12px 'Times New Roman';
+  color: #000000;
+  min-height: 15px;
+`;
+
+export const P17 = styled.p`
+  margin: 0px 0px 12px 0px;
+  text-align: justify;
+  font: 12px 'Times New Roman';
+  color: #000000;
+`;
+
+export const P18 = styled.p`
+  margin: 0px 0px 0px 0px;
+  text-align: justify;
+  text-indent: 36px;
+  font: 12px Times;
+  color: #000000;
+  min-height: 14px;
+`;
+
+export const P20 = styled.p`
+  margin: 0px 0px 6px 72px;
+  text-align: justify;
+  text-indent: -36px;
+  font: 12px 'Times New Roman';
+  color: #000000;
+`;
+
+export const P22 = styled.p`
+  margin: 0px 0px 16px 0px;
+  text-align: justify;
+  text-indent: 36px;
+  font: 12px 'Times New Roman';
+  color: #000000;
+`;
+
+export const P23 = styled.p`
+  margin: 0px 0px 0px 0px;
+  line-height: 15px;
+  font: 12px 'Times New Roman';
+  color: #000000;
+`;
+
+export const P26 = styled.p`
+  margin: 0px 0px 0px 0px;
+  line-height: 15px;
+  font: 12px 'Times New Roman';
+  color: #000000;
+  min-height: 15px;
+`;
+
+export const Li17 = styled.li`
+  margin: 0px 0px 12px 0px;
+  text-align: justify;
+  font: 12px 'Times New Roman';
+  color: #000000;
+`;
+
+export const Li19 = styled.li`
+  margin: 0px 0px 6px 0px;
+  text-align: justify;
+  font: 12px 'Times New Roman';
+  color: #000000;
+`;
+
+export const Li21 = styled.li`
+  margin: 0px 0px 6px 0px;
+  font: 12px 'Times New Roman';
+  color: #000000;
+`;
+
+export const Li23 = styled.li`
+  margin: 0px 0px 0px 0px;
+  line-height: 15px;
+  font: 12px 'Times New Roman';
+  color: #000000;
+`;
+
+export const Li24 = styled.li`
+  margin: 0px 0px 16px 0px;
+  line-height: 15px;
+  font: 12px 'Times New Roman';
+  color: #000000;
+`;
+
+export const Li25 = styled.li`
+  margin: 0px 0px 12px 0px;
+  line-height: 15px;
+  font: 12px 'Times New Roman';
+  color: #000000;
+`;
+
+export const S1 = styled.span`
+  text-decoration: underline;
+`;
+
+export const S2 = styled.span`
+  font: 12px 'Times New Roman';
+  text-decoration: underline;
+`;
+
+export const S3 = styled.span`
+  font: 11px Helvetica;
+`;
+
+export const S4 = styled.span`
+  background-color: #ffffff;
+`;
+
+export const S5 = styled.span`
+  color: #000000;
+`;
+
+export const S6 = styled.span`
+  font: 12px Times;
+  text-decoration: underline;
+`;
+
+export const S7 = styled.span`
+  font: 12px Helvetica;
+`;
+
+export const S8 = styled.span`
+  font: 8px 'Times New Roman';
+`;
+
+export const AppleTab = styled.span`
+  white-space: pre;
+`;
+
+export const Ol1 = styled.ol`
+  list-style-type: decimal;
+`;
+
+export const Ul1 = styled.ul`
+  list-style-type: disc;
+`;
