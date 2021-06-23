49c1a08adf901b37f08f75c9cf2cd937011cbdf9
Fede <federicoalvarez710@gmail.com>
2021-06-23 10:05:57 -0300

    fixi

--- src/views/MarketPlace/components/Filters/SearchInput/index.tsx
+++ src/views/MarketPlace/components/Filters/SearchInput/index.tsx
@@ -44,7 +44,7 @@ const SearchInput = ({ handleFilter, activeFilters }: IProps) => {
           name="search"
           value={searchValue}
           type="text"
-          placeholder="Search1"
+          placeholder="Search"
         />
       </InputDiv>
     </>
