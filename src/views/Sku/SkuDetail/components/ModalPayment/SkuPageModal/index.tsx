77444758d47e79b25ee01d1d20a45e8bd84ad967
Matt Zamora <66799749+MattZ-2051@users.noreply.github.com>
2021-06-24 08:45:56 -0600

    fix sku page error when user is not logged in (#634)

--- src/views/Sku/SkuDetail/components/ModalPayment/SkuPageModal/index.tsx
+++ src/views/Sku/SkuDetail/components/ModalPayment/SkuPageModal/index.tsx
@@ -198,7 +198,7 @@ const SkuPageModal = ({
               <S.Title>Confirm your order:</S.Title>
               <S.SubTitle>
                 {' '}
-                Your current balance ${userBalance.toFixed(2)}
+                Your current balance ${Number(userBalance || 0).toFixed(2)}
               </S.SubTitle>
             </>
           )}
@@ -209,7 +209,7 @@ const SkuPageModal = ({
                 <S.Title> Whoops, Insufficient funds!</S.Title>
               </S.ContentIconTitle>
               <S.SubTitle style={{ color: '#E74C3C' }}>
-                Your wallet balance is $ {userBalance.toFixed(2)}
+                Your wallet balance is ${Number(userBalance || 0).toFixed(2)}
               </S.SubTitle>
             </>
           )}
