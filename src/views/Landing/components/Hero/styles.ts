a3297dee07d039488edfe4352c5f664b3bd97677
Matt Zamora <66799749+MattZ-2051@users.noreply.github.com>
2021-07-21 13:50:57 -0600

    add assets and new landing hero (#830)

--- src/views/Landing/components/Hero/styles.ts
+++ src/views/Landing/components/Hero/styles.ts
@@ -2,53 +2,129 @@ import styled from 'styled-components/macro';
 
 export const Container = styled.section`
   background-color: black;
+  margin: auto;
   position: relative;
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  padding: 80px;
+  flex-direction: row;
   text-align: center;
-  padding: 55px 24px 0 24px;
   color: white;
   padding-bottom: 100px;
-
+  max-width: 1440px;
   @media screen and (max-width: 960px) {
     font-size: 0.9rem;
+    padding: 24px;
     padding-bottom: 50px;
+    flex-direction: column;
   }
 `;
 
-export const Title = styled.h1`
-  font-size: 56px;
-  font-weight: 700;
-  margin: 50px 0 24px 0;
+export const DropButtonContainer = styled.div<{
+  paddingLeft: string;
+  paddingTop: string;
+}>`
+  display: flex;
+  align-items: center;
+  padding-left: ${(props) => `${props.paddingLeft}`};
+  padding-top: ${(props) => `${props.paddingTop}`};
+`;
 
+export const DropButton = styled.span`
+  font-size: 16px;
+  color: #9da1a8;
+  font-weight: 700;
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+export const Button = styled.button<{ width: string }>`
+  width: ${(props) => `${props.width}`};
+  height: 48px;
+  color: white;
+  font-weight: 600;
+  font-size: 16px;
+  background-color: black;
+  border: 1px solid #3a3a3a;
+  border-radius: 35px;
+  :focus {
+    outline: none;
+    border: none;
+  }
+  :hover {
+    cursor: pointer;
+    background-color: #ddf874;
+    color: black;
+  }
+`;
+
+export const ImgContainer = styled.div`
+  position: relative;
+  @media screen and (max-width: 960px) {
+    margin-bottom: 50px;
+  }
+`;
+
+export const DropImg = styled.img`
+  position: absolute;
+  right: 0;
+  left: 44%;
+  top: 78%;
+  width: 305px;
+  @media screen and (max-width: 960px) {
+    width: 235px;
+    left: 38%;
+  }
+  @media screen and (max-width: 400px) {
+    width: 230px;
+    left: 35%;
+  }
+  @media screen and (max-width: 330px) {
+    width: 210px;
+    left: 30%;
+  }
+`;
+
+export const SubContainer = styled.div<{ order: number; padding?: string }>`
+  order: ${(props) => `${props.order}`};
+  padding: ${(props) => `${props.padding}`};
+`;
+
+export const Title = styled.h1<{ fontSize: string }>`
+  font-size: ${(props) => `${props.fontSize}`};
+  font-weight: 700;
+  margin: 0;
+  // margin: 50px 0 24px 0;
+  text-align: left;
   span {
-    background: linear-gradient(45deg, #ff9412 0%, #fff72d 98.96%);
+    background: #ddf874;
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
   }
-
   @media screen and (max-width: 600px) {
-    margin: 50px 0 24px 0;
-    font-size: 40px;
-    margin-bottom: 10%;
-    // word-spacing: 100vw;
+    margin: 44px 0 18px 0;
+    font-size: 32px;
+    text-align: center;
     span {
-      font-size: 40px;
+      font-size: 32px;
     }
   }
 `;
 
-export const Subtitle = styled.h2`
-  font-weight: 500;
+export const Subtitle = styled.h2<{
+  color: string;
+  fontSize: string;
+  fontWeight: number;
+}>`
+  font-weight: ${(props) => `${props.fontWeight}`};
   font-size: 18px;
-  color: #8e8e8e;
-  margin-bottom: 32px;
+  color: ${(props) => `${props.color}`};
   line-height: 32px;
-
+  text-align: left;
+  margin: 0;
   @media screen and (max-width: 600px) {
-    margin-bottom: 12%;
+    // margin-bottom: 12%;
+    text-align: center;
   }
 `;
 
@@ -58,13 +134,11 @@ export const ArrowButton = styled.div`
   bottom: 90px;
   display: flex;
   justify-content: center;
-
   svg {
     font-size: 10rem;
     position: absolute;
     cursor: pointer;
   }
-
   svg + svg {
     top: 29px;
     cursor: pointer;
