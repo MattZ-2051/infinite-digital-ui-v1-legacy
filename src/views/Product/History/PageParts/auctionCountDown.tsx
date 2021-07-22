b55ab4c566a8255bc25ce9b986d582ea7d193da6
Matt Zamora <mzamora2899@gmail.com>
2021-07-22 16:19:03 -0600

    hotfix - fix mobile views for auction

--- src/views/Product/History/PageParts/auctionCountDown.tsx
+++ src/views/Product/History/PageParts/auctionCountDown.tsx
@@ -1,21 +1,37 @@
 import * as S from '../styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
-
+import { formatDate } from 'utils/dates';
 export const AuctionCountDown = ({ product, countdown }) => {
   const matchesMobile = useMediaQuery('(max-width:1140px)');
 
   return (
-    <S.FlexDiv
-      justifyContent={matchesMobile ? 'center' : 'flex-end'}
-      padding={matchesMobile ? '0 0 25px 0' : '0 0 10px 0'}
-    >
-      {' '}
-      <S.Text color="#9e9e9e" size="18px" fontWeight={600}>
-        Expires in
-      </S.Text>
-      <S.Text color="white" size="18px" fontWeight={600}>
-        {product?.activeProductListings[0] && countdown}
-      </S.Text>{' '}
-    </S.FlexDiv>
+    <>
+      <S.FlexDiv
+        justifyContent={matchesMobile ? 'center' : 'flex-end'}
+        padding={matchesMobile ? '15px 0 0 0' : '0 0 10px 0'}
+      >
+        {' '}
+        <S.Text color="#9e9e9e" size="18px" fontWeight={600}>
+          Expires in
+        </S.Text>
+        <S.Text color="white" size="18px" fontWeight={600}>
+          {product?.activeProductListings[0] && countdown}
+        </S.Text>{' '}
+      </S.FlexDiv>
+      {matchesMobile && (
+        <S.Text
+          color="#7c7c7c"
+          size="14px"
+          fontWeight={400}
+          textAlign="center"
+          style={{ padding: '10px 0 35px 0' }}
+        >
+          {product?.activeProductListings[0] &&
+            `(${formatDate(
+              new Date(product?.activeProductListings[0].endDate)
+            )})`}
+        </S.Text>
+      )}
+    </>
   );
 };
