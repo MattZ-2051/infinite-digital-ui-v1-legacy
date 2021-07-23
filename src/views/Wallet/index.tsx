8d647eeea2d479270a671fc79f436cd45ae5ee4c
Matt Zamora <mzamora2899@gmail.com>
2021-07-23 15:00:22 -0600

    re-enable wallet withdrawal

--- src/views/Wallet/index.tsx
+++ src/views/Wallet/index.tsx
@@ -187,16 +187,13 @@ const Wallet = (props) => {
               amount={getUserWithdrawableBalance()}
               subtitle={'(Excludes pending transactions)'}
             />
-
-            <S.ToolTip data-text="Withdrawals will be enabled soon.">
-              <ButtonTextAndImage
-                disabled={true}
-                text={'Withdrawal'}
-                handlerOnClick={handleOpenWithdraw}
-                hoverIcon={withdrawIconBlack}
-                normalIcon={withdrawIconWhite}
-              />
-            </S.ToolTip>
+            <ButtonTextAndImage
+              disabled={false}
+              text={'Withdrawal'}
+              handlerOnClick={handleOpenWithdraw}
+              hoverIcon={withdrawIconBlack}
+              normalIcon={withdrawIconWhite}
+            />
 
             <MuiDivider style={{ margin: '32px 0 24px 0', width: '89%' }} />
 
