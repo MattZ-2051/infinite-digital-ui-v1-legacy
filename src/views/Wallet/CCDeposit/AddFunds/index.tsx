9fd7a82cdbf2396dffefcab8ab37b390ba597368
Joel Del Cueto <59443175+wayfaringjou@users.noreply.github.com>
2021-08-04 12:53:24 -0400

    Add style rules to follow figma, refactor funds input handling (#960)

--- src/views/Wallet/CCDeposit/AddFunds/index.tsx
+++ src/views/Wallet/CCDeposit/AddFunds/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { PulseLoader } from 'react-spinners';
 import circleIcon from 'assets/img/icons/circle-icon-deposit.png';
+import exitIcon from 'assets/img/icons/exit-icon.png';
 import 'react-credit-cards/es/styles-compiled.css';
 import { Padding } from '../styles';
 import { useAppDispatch, useAppSelector } from 'store/hooks';
@@ -16,7 +17,12 @@ import Toast from 'utils/Toast';
 import * as S from './styles';
 import { config } from 'config';
 import { ApiLogicError } from '../../../../utils/apiError';
-import {Card, CVVVerificationStatus, VerificationStatusEnum} from "../../../../entities/card";
+import {
+  Card,
+  CVVVerificationStatus,
+  VerificationStatusEnum,
+} from '../../../../entities/card';
+import Cards from 'react-credit-cards';
 
 const ccDepositLimit = config.kycLimits.ccDepositLimit;
 const dailyDepositLimitMsgRe =
@@ -42,14 +48,14 @@ function ccStatusActiveString(userCard: Card) {
 }
 
 function ccStatusIsActive(obj: Card) {
-  return obj.status === VerificationStatusEnum.complete &&
-  obj.verification?.cvv === CVVVerificationStatus.pass;
+  return (
+    obj.status === VerificationStatusEnum.complete &&
+    obj.verification?.cvv === CVVVerificationStatus.pass
+  );
   // &&
   // obj.verification?.avs === AVSCode.A &&
   // !obj.riskEvaluation
 }
-
-const zeros = ['0', '0.00', '.00', '', '00.00', '0.000', '0...00', '0.0..00'];
 
 function showDepositToastMessage(depositErrMsg) {
   const matchDaily = dailyDepositLimitMsgRe.exec(depositErrMsg);
@@ -69,27 +75,25 @@ function showDepositToastMessage(depositErrMsg) {
   Toast.error(`Other deposit error`);
 }
 
-const AddFunds = () => {
+const AddFunds = (): JSX.Element | null => {
   const userCard = useAppSelector((state) => state.session.userCards.cards[0]);
   const history = useHistory();
   const dispatch = useAppDispatch();
   const { getAccessTokenSilently } = useAuth0();
-  const [amount, setAmount] = useState<string>('');
+  const [amount, setAmount] = useState<string | null>(null);
   const [valueCvv, setCvv] = useState<string>('');
-  useEffect(
-    () => {
-      if (!userCard?.id) {
-        history.push(`/wallet/addcreditcard`);
-      }
-    },
-    [userCard?.id]
-  );
+  useEffect(() => {
+    if (!userCard?.id) {
+      history.push(`/wallet/addcreditcard`);
+    }
+  }, [userCard?.id]);
 
-  const activeButton = !zeros.includes(amount || '') && isValidCvv(valueCvv);
+  const parsedAmount = amount ? parseFloat(amount) : 0;
+  const activeButton =
+    amount !== null && parsedAmount > 0 && isValidCvv(valueCvv);
   const addFunds = async () => {
     if (!activeButton) return;
-    const lAmount = amount?.replace(',', '').replace(/^0+/, '');
-    if (amount && parseFloat(amount.replaceAll(',', '')) > ccDepositLimit) {
+    if (amount !== null && parsedAmount > ccDepositLimit) {
       Toast.error(
         `You can only deposit up to $${ccDepositLimit} USD per credit card transaction`
       );
@@ -101,7 +105,7 @@ const AddFunds = () => {
         token: userToken,
         data: {
           email: userCard.metadata.email,
-          amount: lAmount,
+          amount: parsedAmount?.toFixed(2),
           cvv: valueCvv,
         },
         cardId: userCard.id,
@@ -154,7 +158,6 @@ const AddFunds = () => {
   const month = userCard.expMonth.toString();
 
   const expDate = (month.length === 1 ? '0' + month : month) + '/' + year;
-
   return (
     <S.Container>
       <S.ContentContainer>
@@ -163,18 +166,23 @@ const AddFunds = () => {
         >
           <S.HeaderDiv>
             <img src={circleIcon} alt="" />
-            <S.HeaderText>Circle Payments</S.HeaderText>
+            <S.HeaderText>Select your credit card</S.HeaderText>
           </S.HeaderDiv>
+          <S.ExitIcon
+            src={exitIcon}
+            alt=""
+            onClick={() => history.push('/wallet')}
+          />
         </S.Row>
         <Padding>
           <S.AddFundsText>Add funds into your wallet</S.AddFundsText>
         </Padding>
         <S.CardContainer>
-          <S.CreditCard
+          <Cards
             name=" "
             expiry={expDate}
             focus=""
-            number={`************${userCard.last4}`}
+            number={`••••••••••••${userCard.last4}`}
             preview={true}
             issuer={`${userCard.network}`}
           />
@@ -182,9 +190,33 @@ const AddFunds = () => {
         <S.Row>
           <div>
             <span>Credit Card</span>
-            <S.ActiveText statusStr={ccActiveStatus}>({ccActiveStatus})</S.ActiveText>
+            <S.ActiveText statusStr={ccActiveStatus}>
+              ({ccActiveStatus})
+            </S.ActiveText>
           </div>
           <S.RemoveCCButton onClick={removeCard}>Remove Card</S.RemoveCCButton>
+        </S.Row>
+        <S.Row
+          style={{
+            justifyContent: 'center',
+            paddingTop: 25,
+            paddingBottom: 10,
+          }}
+        >
+          <S.FormInput
+            onChange={(ev) => {
+              const vv = ev.target.value;
+              if (!isPartialValidCvv(vv)) {
+                return;
+              }
+              setCvv(vv);
+            }}
+            value={valueCvv}
+            // label="CVV"
+            inputProps={{ maxLength: 3 }}
+            placeholder="Enter CVV"
+            // maxLength={3}
+          />
         </S.Row>
         <S.FeeReminderContainer>
           <S.FeeReminderText>
@@ -199,39 +231,15 @@ const AddFunds = () => {
             name="amount-input"
             placeholder="Enter Amount"
             decimalsLimit={2}
-            onChange={(e) => {
-              if (e.target.value.split('.').length !== 2) {
-                setAmount(e.target.value + '.00');
-              } else {
-                setAmount(e.target.value || '');
-              }
-            }}
+            onValueChange={(value) => setAmount(value ? value : null)}
             maxLength={10}
             step={10}
+            value={amount ?? ''}
             defaultValue={0.0}
             allowNegativeValue={false}
           />
         </S.AmountContainer>
-        <S.Row style={{
-          justifyContent: 'center',
-          paddingTop: 25,
-          paddingBottom: 10,
-        }}>
-          <S.FormInput
-            onChange={(ev) => {
-              const vv = ev.target.value;
-              if (!isPartialValidCvv(vv)) {
-                return;
-              }
-              setCvv(vv);
-            }}
-            value={valueCvv}
-            // label="CVV"
-            inputProps={{ maxLength: 3 }}
-            placeholder="CVV"
-            // maxLength={3}
-          />
-        </S.Row>
+
         <Padding>
           {activeButton && ccIsActive ? (
             <S.AddFundsButton
