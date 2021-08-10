e7b063e86d5c03ccf728a1a8025b7b5e5d1c0f7b
Matt Zamora <mzamora2899@gmail.com>
2021-08-10 15:17:17 -0600

    add redirect to marketplace on button click

--- src/views/Landing/components/SkuTilesTab/index.tsx
+++ src/views/Landing/components/SkuTilesTab/index.tsx
@@ -1,9 +1,11 @@
 import LatestReleases from './components/LatestReleases';
 import * as S from './styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useHistory } from 'react-router';
 
 const SkuTilesTab = (): JSX.Element => {
   const matchesMobile = useMediaQuery('(max-width:960px)', { noSsr: true });
+  const history = useHistory();
 
   const DeskTopView = ({ matchesMobile }) => {
     return (
@@ -12,7 +14,9 @@ const SkuTilesTab = (): JSX.Element => {
         <S.Header>Welcome to the Megaverse, to Infinite World</S.Header>
         <LatestReleases matchesMobile={matchesMobile} />
         <S.FlexDiv>
-          <S.MarketPlaceButton>Explore The Marketplace</S.MarketPlaceButton>
+          <S.MarketPlaceButton onClick={() => history.push('/marketplace')}>
+            Explore The Marketplace
+          </S.MarketPlaceButton>
         </S.FlexDiv>
       </S.Container>
     );
@@ -30,7 +34,9 @@ const SkuTilesTab = (): JSX.Element => {
           Megaverse <br /> to Infinite World
         </S.Header>
         <S.FlexDiv>
-          <S.MarketPlaceButton>Explore The Marketplace</S.MarketPlaceButton>
+          <S.MarketPlaceButton onClick={() => history.push('/marketplace')}>
+            Explore The Marketplace
+          </S.MarketPlaceButton>
         </S.FlexDiv>
         <LatestReleases matchesMobile={matchesMobile} />
       </S.Container>
