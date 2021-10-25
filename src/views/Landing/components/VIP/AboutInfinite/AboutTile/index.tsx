00a52112e8ba654e34177fdce17e21795374de52
Matt Zamora <mzamora2899@gmail.com>
2021-10-25 10:39:25 -0600

    hotfix - images on vip page

--- src/views/Landing/components/VIP/AboutInfinite/AboutTile/index.tsx
+++ src/views/Landing/components/VIP/AboutInfinite/AboutTile/index.tsx
@@ -1,26 +1,35 @@
 import * as S from './styles';
-import BrandsPageImg from 'assets/VIPLanding/BrandsPageMobile.png';
-import MarketplaceImg from 'assets/VIPLanding/MarketplaceMobile.png';
-import ThreeDWorldImg from 'assets/VIPLanding/3dWorldMobile.png';
+import BrandsPageImgMobile from 'assets/VIPLanding/BrandsPageMobile.png';
+import BrandsPageImg from 'assets/VIPLanding/BrandsPage.png';
+import MarketplaceImgMobile from 'assets/VIPLanding/MarketplaceMobile.png';
+import MarketplaceImg from 'assets/VIPLanding/MarketplaceImg.png';
+import ThreeDWorldImgMobile from 'assets/VIPLanding/3dWorldMobile.png';
+import ThreeDWorldImg from 'assets/VIPLanding/3dWorldImg.png';
 import { AboutTileProps } from '../index';
 import { useMediaQuery } from '@material-ui/core';
 
 const TileImg = ({ category }) => {
+  const matchesMobile = useMediaQuery('(max-width:640px)', { noSsr: true });
+
+  const brandsImg = matchesMobile ? BrandsPageImgMobile : BrandsPageImg;
+  const marketplaceImg = matchesMobile ? MarketplaceImgMobile : MarketplaceImg;
+  const threeDImg = matchesMobile ? ThreeDWorldImgMobile : ThreeDWorldImg;
+
   return (
     <>
       {category === 'Products' && (
         <S.ImgContainer>
-          <S.Img src={BrandsPageImg} />
+          <S.Img src={brandsImg} />
         </S.ImgContainer>
       )}
       {category === 'Marketplace' && (
         <S.ImgContainer>
-          <S.Img src={MarketplaceImg} />
+          <S.Img src={marketplaceImg} />
         </S.ImgContainer>
       )}
       {category === '3d Worlds' && (
         <S.ImgContainer>
-          <S.Img src={ThreeDWorldImg} />
+          <S.Img src={threeDImg} />
         </S.ImgContainer>
       )}
     </>
