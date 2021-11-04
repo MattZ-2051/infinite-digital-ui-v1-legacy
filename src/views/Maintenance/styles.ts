8554f1d0f11f03f5653de3c9f105cca2adb5e290
Matt Zamora <66799749+MattZ-2051@users.noreply.github.com>
2021-11-04 09:42:09 -0600

    feat(app): add maintenance page and route (#1120)

---
+++ src/views/Maintenance/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components/macro';
+
+export const Container = styled.div`
+  display: flex;
+  height: 70%;
+  justify-content: center;
+  align-items: center;
+  max-width: 1440px;
+  margin: auto;
+`;
+
+export const Title = styled.h1`
+  font-weight: 700;
+  margin: 2rem 0;
+`;
+
+export const SubTitle = styled.h2`
+  font-weight: 400;
+`;
