48d97b51fe0460441bb4f2490d61b1781b44fd3f
Matt Zamora <mzamora2899@gmail.com>
2021-12-20 09:50:56 -0700

    re-enable maintenance

--- src/views/Maintenance/index.tsx
+++ src/views/Maintenance/index.tsx
@@ -8,11 +8,6 @@ const Maintenance = () => {
           <S.Title>
             {"We're re down for maintenance and will be right back!"}
           </S.Title>
-          <S.SubTitle>
-            The Hedera Hashgraph network is being updated, please check{' '}
-            <a href={'https://status.hedera.com/'}>hedera</a> for the latest
-            status.
-          </S.SubTitle>
         </div>
       </S.Container>
     </div>
